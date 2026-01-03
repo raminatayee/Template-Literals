fb2aaa07b8f06253ecb6dfc00fde3579c01e5c94
Ramin Atayee <raminatayeeofficial@gmail.com>
2026-01-04 00:57:56 +0430

    Template Literals

---
+++ main.js
@@ -0,0 +1,11 @@
+const price = 300;
+const name = "کتاب";
+console.log(" قیمت محصول \n\
+    "+ name + " مبلغ \n\
+    " + price + " است ");
+
+    console.log(`قیمت محصول ${name} ${price} است`)
+
+    console.log(`قیمت محصول 
+        ${name}
+         ${price} است`)
